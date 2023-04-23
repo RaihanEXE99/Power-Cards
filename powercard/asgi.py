453e52117bf156fa1e72fbfca16192b00a1c2a01
RaihanEXE99 <RaihanEXE99#gmail.com>
2023-04-23 18:32:23 +0600

    wss 2 asgi

--- powercard/asgi.py
+++ powercard/asgi.py
@@ -16,14 +16,14 @@
 # application = get_asgi_application()
 
 import os
-
+import django
 from django.core.asgi import get_asgi_application
 from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
 import app.routing
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'powercard.settings')
-
+django.setup()
 # application = get_asgi_application()
 application = ProtocolTypeRouter({
     "http": get_asgi_application(),
